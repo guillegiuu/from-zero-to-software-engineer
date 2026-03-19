72f55ffb9857bdb87021f0d87c2b2be2ca8509c2
Guille Giu | Computer Science <115334202+guillegiuu@users.noreply.github.com>
2026-03-19 10:30:48 -0300

    Add boolean logic example for Chapter 2

---
+++ code_examples/chapter2/boolean_logic.py
@@ -0,0 +1,10 @@
+# Ejemplo de lógica booleana con and
+
+age = 20
+has_id = True
+
+# Verificamos si puede entrar
+if age >= 18 and has_id:
+    print("Access granted")
+else:
+    print("Access denied")
