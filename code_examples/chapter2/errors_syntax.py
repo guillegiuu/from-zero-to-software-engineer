caf15180d824cd94a2f02af29915ab6d3617f7c2
Guille Giu | Computer Science <115334202+guillegiuu@users.noreply.github.com>
2026-03-22 17:17:45 -0300

    Create errors_syntax.py

---
+++ code_examples/chapter2/errors_syntax.py
@@ -0,0 +1,7 @@
+# ❌ SYNTAX ERROR
+# Error en la estructura del código
+
+# if 5 > 3
+#     print("Esto falla")  # Falta ":" en el if
+
+# 👉 Este código no se ejecuta directamente porque rompe el programa
