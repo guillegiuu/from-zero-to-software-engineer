a17fb81ebff812c16010d6636c71341e2d52acad
Guille Giu | Computer Science <115334202+guillegiuu@users.noreply.github.com>
2026-03-29 19:21:46 -0300

    add create_list example for chapter 4

---
+++ code_examples/chapter4/create_list.py
@@ -0,0 +1,10 @@
+# Crear listas en Python
+
+# Lista de números
+numeros = [1, 2, 3, 4]
+
+# Lista con diferentes tipos
+datos = [10, "hola", True]
+
+print("Lista de números:", numeros)
+print("Lista con distintos tipos:", datos)
