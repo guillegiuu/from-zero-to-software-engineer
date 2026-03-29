b46dc54b1101f781cbe22696327c7208b03fe2e4
Guille Giu | Computer Science <115334202+guillegiuu@users.noreply.github.com>
2026-03-29 15:58:04 -0300

    add chapter 3 code examples and structure v2

---
+++ code_examples/chapter3/data_types.py
@@ -0,0 +1,11 @@
+# Tipos de datos
+
+a = 10          # int
+b = 3.14        # float
+c = "Hola"      # string
+d = True        # boolean
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
