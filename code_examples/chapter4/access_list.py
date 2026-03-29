7d52c2a165f9a2fcb06aa3a2fe53ac131c52dcc2
Guille Giu | Computer Science <115334202+guillegiuu@users.noreply.github.com>
2026-03-29 19:24:06 -0300

    add access_list example for chapter 4

---
+++ code_examples/chapter4/access_list.py
@@ -0,0 +1,10 @@
+# Acceso a elementos de una lista
+
+frutas = ["manzana", "banana", "naranja"]
+
+# Acceso por índice
+print("Primer elemento:", frutas[0])
+print("Segundo elemento:", frutas[1])
+
+# Índice negativo (desde el final)
+print("Último elemento:", frutas[-1])
