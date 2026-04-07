9b37baaa0de2df3c08e79c9858172e6f8de77258
Guille Giu | Computer Science <115334202+guillegiuu@users.noreply.github.com>
2026-04-07 01:10:29 -0300

    add parameters example for chapter 6

---
+++ code_examples/chapter6/parameters.py
@@ -0,0 +1,9 @@
+# parameters.py
+# Ejemplo de uso de parámetros en funciones
+
+def saludar(nombre):
+    print("Hola,", nombre)
+
+# Llamadas con distintos argumentos
+saludar("Guille")
+saludar("Python")
