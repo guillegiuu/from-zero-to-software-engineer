db019462859baece7fd98eed9c35ad9e6a17e103
Guille Giu | Computer Science <115334202+guillegiuu@users.noreply.github.com>
2026-04-07 01:12:37 -0300

    add return example for chapter 6

---
+++ code_examples/chapter6/return_example.py
@@ -0,0 +1,10 @@
+# return_example.py
+# Ejemplo de uso de return en funciones
+
+def sumar(a, b):
+    return a + b
+
+# Guardamos el resultado
+resultado = sumar(2, 3)
+
+print("Resultado:", resultado)
