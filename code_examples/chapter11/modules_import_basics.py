0e6ad4d30e1b9b0375a604ca5095e858223b97c1
Guille Giu | Computer Science <115334202+guillegiuu@users.noreply.github.com>
2026-04-15 13:36:17 -0300

    add modules_import_basics example (import, from, alias usage)

---
+++ code_examples/chapter11/modules_import_basics.py
@@ -0,0 +1,16 @@
+# 📦 Modules - Import Basics
+
+# Importar todo el módulo
+import math
+
+print("Raíz cuadrada de 9:", math.sqrt(9))
+
+# Importar función específica
+from math import sqrt
+
+print("Raíz cuadrada de 16:", sqrt(16))
+
+# Usar alias
+import math as m
+
+print("Raíz cuadrada de 25:", m.sqrt(25))
