51d58aea2a53fe2eacdbf4499a87eea9392ce10a
Guille Giu | Computer Science <115334202+guillegiuu@users.noreply.github.com>
2026-04-15 13:36:42 -0300

    add random_module_usage example (randint and choice)

---
+++ code_examples/chapter11/random_module_usage.py
@@ -0,0 +1,11 @@
+# 🎲 Random Module
+
+import random
+
+# Número aleatorio entre 1 y 10
+print("Número aleatorio:", random.randint(1, 10))
+
+# Elegir elemento de una lista
+frutas = ["manzana", "banana", "pera"]
+
+print("Fruta aleatoria:", random.choice(frutas))
