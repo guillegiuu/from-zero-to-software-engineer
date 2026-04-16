23fb45639e2f3e89a81d916b3225507052f18061
Guille Giu | Computer Science <115334202+guillegiuu@users.noreply.github.com>
2026-04-16 00:35:35 -0300

    git commit -m "feat: add solution for control flow challenge 02"

---
+++ challenges/control_flow_basic/solutions/challenge_02.py
@@ -0,0 +1,14 @@
+# 🧠 Challenge 02: Over Budget
+
+budget = 2000
+food_bill = 250
+electricity_bill = 120
+internet_bill = 60
+rent = 1500
+
+total = food_bill + electricity_bill + internet_bill + rent
+
+over_budget = total > budget
+
+print(over_budget)
+
