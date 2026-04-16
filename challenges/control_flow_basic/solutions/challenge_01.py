24775681d6c68a37d29b28690313df6820232f19
Guille Giu | Computer Science <115334202+guillegiuu@users.noreply.github.com>
2026-04-16 00:58:34 -0300

    Update challenge_01.py edit 1

--- challenges/control_flow_basic/solutions/challenge_01.py
+++ challenges/control_flow_basic/solutions/challenge_01.py
@@ -1,15 +1,11 @@
 # 🧠 Challenge 01: Not Sum To Ten
 
-# Input
 num1 = 6
 num2 = 3
 
-# Lógica:
-# Verificar si la suma NO es igual a 10
 if (num1 + num2) != 10:
     not_ten = True
 else:
     not_ten = False
 
-# Output
 print(not_ten)
