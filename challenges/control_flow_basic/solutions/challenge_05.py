9a8b1170094796973b288a7c1da06b865d44fca7
Guille Giu | Computer Science <115334202+guillegiuu@users.noreply.github.com>
2026-04-16 00:51:42 -0300

    git commit -m "feat: add solution for control flow challenge 05"

---
+++ challenges/control_flow_basic/solutions/challenge_05.py
@@ -0,0 +1,11 @@
+# 🧠 Challenge 05: Divisible By Ten
+
+def divisible_by_ten(num):
+    if num % 10 == 0:
+        return True
+    else:
+        return False
+
+
+print(divisible_by_ten(20))
+print(divisible_by_ten(25))
