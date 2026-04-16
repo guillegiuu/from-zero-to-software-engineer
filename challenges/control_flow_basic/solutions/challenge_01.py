6f8901cc7f9fbc38058632ee7d90541537cc7074
Guille Giu | Computer Science <115334202+guillegiuu@users.noreply.github.com>
2026-04-16 00:16:39 -0300

    git commit -m "refactor: simplify solution file for better readability and clean code structure"

---
+++ challenges/control_flow_basic/solutions/challenge_01.py
@@ -0,0 +1,15 @@
+# 🧠 Challenge 01: Not Sum To Ten
+
+# Input
+num1 = 6
+num2 = 3
+
+# Lógica:
+# Verificar si la suma NO es igual a 10
+if (num1 + num2) != 10:
+    not_ten = True
+else:
+    not_ten = False
+
+# Output
+print(not_ten)
