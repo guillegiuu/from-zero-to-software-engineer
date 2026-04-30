83039de8afd973ddd262c748459f727ea58107aa
Guille Giu | Computer Science <115334202+guillegiuu@users.noreply.github.com>
2026-04-30 00:34:00 -0300

    add dict basics example (creation and modification)

---
+++ code_examples/chapter12/dict_basics.py
@@ -0,0 +1,15 @@
+# Creación y modificación de diccionarios
+
+# Crear diccionario
+usuario = {
+    "nombre": "Guille",
+    "edad": 25
+}
+
+# Agregar nueva clave
+usuario["pais"] = "Argentina"
+
+# Modificar valor existente
+usuario["edad"] = 26
+
+print(usuario)
