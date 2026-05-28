c4359fd5ac0277046458acd59f2a36e4717c8e65
Guille Giu | Computer Science <115334202+guillegiuu@users.noreply.github.com>
2026-05-28 01:57:09 -0300

    Fix indentation in syntax_1.py
    Correct the indentation of the code block.

---
+++ challenges/python_basics/syntax_1.py
@@ -0,0 +1,9 @@
+# Python Basics — Home
+# Syntax 2
+
+# Este ejercicio pide:
+# Corregir la indentación del código
+
+# La indentación en Python define bloques de código
+if 5 > 2:
+    print("Five is greater than two!")
